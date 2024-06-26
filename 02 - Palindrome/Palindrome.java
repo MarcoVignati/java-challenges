a595d05c681b139495db84b381038ba36db27e58
Marco Vignati <marcovignatidev@gmail.com>
2024-06-26 18:41:34 -0400

    solved challenge 02

--- 02 - Palindrome/Palindrome.java
+++ 02 - Palindrome/Palindrome.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type the word or phrase:");
+        String input = scanner.nextLine();
+        String inputNoSpaces = input.toLowerCase().replaceAll(" ", "").replaceAll("  ", "").trim();
+        String invertedInput = "";
+
+        for (int i = inputNoSpaces.length() - 1; i >= 0; i--) {
+            invertedInput += inputNoSpaces.charAt(i);
+        }
+
+        if (inputNoSpaces.equals(invertedInput)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
